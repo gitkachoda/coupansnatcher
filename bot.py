318583fd4a1275e3a41abcc171ba1697298ecbf9
gitkachoda <kumarsonuravi06@gmail.com>
2025-10-12 18:46:33 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -36,8 +36,8 @@ logging.getLogger().addHandler(console)
 
 # Coupon generator
 def generate_coupon():
-    fixed = "K6GLNG7"
-    random_part = ''.join(random.choices(string.ascii_uppercase, k=5))
+    fixed = "K6"
+    random_part = ''.join(random.choices(string.ascii_uppercase, k=10))
     coupon = fixed + random_part
     logging.info(f"🎟️ Generated Coupon: {coupon}")
     return coupon
